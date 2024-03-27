e8125f817cfffffb6f7dd26bbcddddb509acf537
Elgara5364 <steveloardi5364@gmail.com>
2024-03-27 17:55:25 +0900

    add &apos; on hero components

--- src/app/components/hero/page.js
+++ src/app/components/hero/page.js
@@ -13,7 +13,7 @@ const Hero = () => {
     <section className=" w-full h-[400px] ">
       <div className="flex flex-col justify-center text-center ">
         <h1 className="pt-[100px] lg:pt-[140px] font-bold text-4xl pb-3 text-secondary ">
-          Hi, I'm{" "}
+          Hi, I&apos;m{" "}
         </h1>
 
         <h1 className="text-8xl text-secondary">Steve Loardi</h1>
